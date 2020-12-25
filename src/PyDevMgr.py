3faeb28426081f046007de7f2acb4b2141218d94
Chuck McKnight <chuckmcknight@pheonic.com>
2020-12-25 13:48:16 -0800

    Added custom wx.App to account for missing functionality

--- src/PyDevMgr.py
+++ src/PyDevMgr.py
@@ -9,8 +9,7 @@ class MainFrame(wx.Frame):
     Main frame for the PyDevMgr application
     """
     def __init__(self):
-        super().__init__(None, title='pylaunch', style=wx.CAPTION, size=wx.Size(300,600))
-
+        super().__init__(None, title='Python Dev Manager', size=wx.Size(300,600))
 
         panel = MainPanel(self)
         self.Bind(wx.EVT_BUTTON, self.__on_close, id=wx.ID_EXIT)
@@ -19,7 +18,27 @@ class MainFrame(wx.Frame):
     def __on_close(self, event):
         self.Destroy()
 
+class PyDevMgrApp(wx.App):
+    """
+    PyDevMgrApp - Custom wxApp class
+    """
+    def __init__(self):
+        super().__init__(redirect=False)
+
+    """
+    MacReopenApp - Allows minimized app to be restored from taskbar
+    """
+    def MacReopenApp(self):
+        top = self.GetTopWindow()
+        if top and top.IsIconized():
+            top.Iconize(False)
+        if top:
+            top.Raise()
+
+
+
 if __name__ == '__main__':
-    app = wx.App(redirect=False)
+    # app = wx.App(redirect=False)
+    app = PyDevMgrApp()
     frame = MainFrame()
     app.MainLoop()
